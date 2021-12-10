691f05539e4b0e4350c4de5082cf0e9a00fb525d
rameshavinash94 <87649563+rameshavinash94@users.noreply.github.com>
2021-12-09 23:17:07 -0800

    Update Ecg.py

--- Deployment/Ecg.py
+++ Deployment/Ecg.py
@@ -230,7 +230,7 @@ class ECG:
 		This Function Loads the pretrained model and perfrom ECG classification
 		return the classification Type.
 		"""
-		loaded_model = joblib.load('model_test (4).pkl')
+		loaded_model = joblib.load('model_test (5).pkl')
 		result = loaded_model.predict(final_df)
 		if result[0] == 1:
 			return "You ECG corresponds to Myocardial Infarction"
